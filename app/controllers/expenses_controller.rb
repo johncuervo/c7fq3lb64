41c7a4e341c4e3e9e1c9478f8e10de792a90396c
John <johnbaironlr@gmail.com>
2019-07-17 14:48:20 -0500

    Configuracion devise y gastos por usuario

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,11 +1,15 @@
 class ExpensesController < ApplicationController
+  before_action :authenticate_user!
+
   def index
-    @expenses = Expense.order("date DESC")
-    if params[:concept].present?
-      @expenses = @expenses.where("concept LIKE ?", "%#{params[:concept]}%")
+    if params[:concept].present? && params[:category_id].present?
+      @expenses = Expense.where("concept like '%#{params[:concept]}%' AND category_id = '#{params[:category_id]}'")
+    elsif params[:concept].present?
+      @expenses = Expense.where("concept like '%#{params[:concept]}%'")
+    elsif params[:category_id].present?
+      @expenses = Expense.where("category_id = ?", params[:category_id])
+    else
+      @expenses = Expense.order("date DESC").where("user_id=?",current_user.id)
     end
-    if params[:category_id].present?
-      @expenses = @expenses.where("category_id = ?", params[:category_id])
-    end
-end
+  end
 end
